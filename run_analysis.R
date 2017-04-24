c8f8f547089d32c5a5d652d1ecafbcffe0517b74
foosa18 <ullaspathak@gmail.com>
2017-04-24 10:42:18 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -12,42 +12,39 @@ if(!file.exists("UCI HAR Dataset")){
  }
 
 #Loading First Level Data
-label_activity<-read.delim("UCI HAR Dataset/activity_labels.txt", header=FALSE, sep="", col.names=c("ID", "ACTIVITY"))
-features<- read.delim("UCI HAR Dataset/features.txt", header=FALSE, sep="", col.names=c("featureID", "featureName"), stringsAsFactors = FALSE)
+# Load activity labels + features
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+activityLabels[,2] <- as.character(activityLabels[,2])
+features <- read.table("UCI HAR Dataset/features.txt")
+features[,2] <- as.character(features[,2])
 
-#Loading the Dataset Train
-training_set<-read.delim("UCI HAR Dataset/train/X_train.txt", header=FALSE, sep="", col.names=features$featureName)
-training_labels<-read.delim("UCI HAR Dataset/train/y_train.txt", header=FALSE, sep="", col.names= c("ID"))
-training_subject<-read.delim("UCI HAR Dataset/train/subject_train.txt", header=FALSE, sep="", col.names=c("SID"))
-train_data<- tbl_df(cbind(training_subject, training_labels, training_set))
+# Extract only the data on mean and standard deviation
+featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
+featuresWanted.names <- features[featuresWanted,2]
+featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
+featuresWanted.names = gsub('-std', 'Std', featuresWanted.names)
+featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
 
-#Loading the Dataset Test
-test_subject<-read.delim("UCI HAR Dataset/test/subject_test.txt", header=FALSE, sep="", col.names=c("SID"))
-test_labels<-read.delim("UCI HAR Dataset/test/y_test.txt", header=FALSE, sep="", col.names=c("ID"))
-test_set<-read.delim("UCI HAR Dataset/test/X_test.txt", header=FALSE, sep="", col.names=features$featureName)
-test_data<-tbl_df(cbind(test_subject, test_labels, test_set))
+# Load the datasets
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
+trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
+trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
 
-#Merging Data
-All_Data<-rbind(train_data, test_data)
-All_colNames<-colnames(All_Data)
-ALL_Data<-All_Data%>%select(SID, ID, contains("mean.."), contains("std.."))
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresWanted]
+testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
+testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(testSubjects, testActivities, test)
 
-#Descriptive names to name activities
-All_Data <- ALL_Data %>% inner_join(label_activity)
-ALL_Data <- All_Data %>% select(SID, ACTIVITY, contains("mean.."), contains("std.."))
-All_Data <- All_Data %>% setNames(gsub("^f", "FrequencyDomain", names(.)))
-All_Data <- All_Data %>% setNames(gsub("^t", "TimeDomain", names(.)))
-All_Data <- All_Data %>% setNames(gsub("Acc", "Accelerometer", names(.)))
-All_Data <- All_Data %>% setNames(gsub("Gyro", "Gyroscope", names(.)))
-All_Data <- All_Data %>% setNames(gsub("Mag", "Magnitude", names(.)))
-All_Data <- All_Data %>% setNames(gsub("mean\\.\\.", "Mean", names(.)))
-All_Data <- All_Data %>% setNames(gsub("std\\.\\.", "Std", names(.)))
-All_Data <- All_Data %>% setNames(gsub("\\.", "", names(.)))
-All_Data <- All_Data %>% select(-starts_with("angle"))
+# merge datasets and add labels
+allData <- rbind(train, test)
+colnames(allData) <- c("subject", "activity", featuresWanted.names)
 
+# turn activities & subjects into factors
+allData$activity <- factor(allData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+allData$subject <- as.factor(allData$subject)
 
-#Second independent tidy data set with average of each variable & Writing Data
-summary_all_data <- All_Data %>% group_by(SID, ACTIVITY)
-summary_all_data <- summary_all_data %>% summarize_each(funs(mean))
-write.table(summary_all_data, file = "tidy_data.txt", row.name = FALSE)
-print(summary_all_data)
+allData.melted <- melt(allData, id = c("subject", "activity"))
+allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
+
+write.table(allData.mean, "tidy.csv", row.names = FALSE, quote = FALSE)
